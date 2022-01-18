c96adfbcbbc365981723419c69a18183d1dbe482
Jang-Sijin <76439399+Jang-Sijin@users.noreply.github.com>
2022-01-19 03:10:34 +0900

    220119_InGameScene_Upload
    1. 인게임 씬 적용
    2. Scene Manager, Sound Manager, Network Manager 구현 및 수정
    3. +@

---
+++ Assets/Manager/NetworkManager.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    [Header("DisconnectPanel")]
+    public GameObject DisconnectPanel;
+    public InputField NickNameInput;
+
+    [Header("LobbyPanel")]
+    public GameObject LobbyPanel;
+    public InputField RoomInput;
+    public Text WelcomeText;
+    public Text LobbyInfoText;
+    public Button[] CellBtn;
+    public Button PreviousBtn;
+    public Button NextBtn;
+
+    [Header("RoomPanel")]
+    public GameObject RoomPanel;
+    public Text ListText;
+    public Text RoomInfoText;
+    public Text[] ChatText;
+    public InputField ChatInput;
+
+    [Header("ETC")]
+    public Text StatusText;
+    public PhotonView PV;
+
+    List<RoomInfo> myList = new List<RoomInfo>();
+    int currentPage = 1, maxPage, multiple;
+    
+    
+    public static NetworkManager instance; // Network Manager을 싱글톤으로 관리
+
+    #region 서버연결
+    // void Awake() => Screen.SetResolution(960, 540, false);
+    private void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        // Sound Manager 싱글톤 설정
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } 
+        else
+        { 
+            Destroy(gameObject);  
+        }
+    }
+
+    void Update()
+    {
+        StatusText.text = PhotonNetwork.NetworkClientState.ToString();
+        LobbyInfoText.text = (PhotonNetwork.CountOfPlayers - PhotonNetwork.CountOfPlayersInRooms) + "로비 / " + PhotonNetwork.CountOfPlayers + "접속";
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster() => PhotonNetwork.JoinLobby();
+
+    public override void OnJoinedLobby()
+    {
+        // 로비 화면
+        LobbyPanel.SetActive(true);
+        RoomPanel.SetActive(false);
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        WelcomeText.text = PhotonNetwork.LocalPlayer.NickName + "님 환영합니다";
+        myList.Clear();
+        
+        // 로비 BGM 재생
+        SoundManager.instance.BackGroundSoundPlay(LobbyPanel);
+    }
+
+    public void Disconnect() => PhotonNetwork.Disconnect();
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        // 로그인 화면
+        LobbyPanel.SetActive(false);
+        RoomPanel.SetActive(false);
+        
+        // 로그인 BGM 재생
+        SoundManager.instance.BackGroundSoundPlay(DisconnectPanel);
+    }
+    #endregion
+
+
+    #region 방리스트 갱신
+    // ◀버튼 -2 , ▶버튼 -1 , 셀 숫자
+    public void MyListClick(int num)
+    {
+        if (num == -2) --currentPage;
+        else if (num == -1) ++currentPage;
+        else PhotonNetwork.JoinRoom(myList[multiple + num].Name);
+        MyListRenewal();
+    }
+    
+    void MyListRenewal()
+    {
+        // 최대페이지
+        maxPage = (myList.Count % CellBtn.Length == 0) ? myList.Count / CellBtn.Length : myList.Count / CellBtn.Length + 1;
+    
+        // 이전, 다음버튼
+        PreviousBtn.interactable = (currentPage <= 1) ? false : true;
+        NextBtn.interactable = (currentPage >= maxPage) ? false : true;
+    
+        // 페이지에 맞는 리스트 대입
+        multiple = (currentPage - 1) * CellBtn.Length;
+        for (int i = 0; i < CellBtn.Length; i++)
+        {
+            CellBtn[i].interactable = (multiple + i < myList.Count) ? true : false;
+            CellBtn[i].transform.GetChild(0).GetComponent<Text>().text = (multiple + i < myList.Count) ? myList[multiple + i].Name : "";
+            CellBtn[i].transform.GetChild(1).GetComponent<Text>().text = (multiple + i < myList.Count) ? myList[multiple + i].PlayerCount + "/" + myList[multiple + i].MaxPlayers : "";
+        }
+    }
+    
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        int roomCount = roomList.Count;
+        for (int i = 0; i < roomCount; i++)
+        {
+            if (!roomList[i].RemovedFromList)
+            {
+                if (!myList.Contains(roomList[i])) myList.Add(roomList[i]);
+                else myList[myList.IndexOf(roomList[i])] = roomList[i];
+            }
+            else if (myList.IndexOf(roomList[i]) != -1) myList.RemoveAt(myList.IndexOf(roomList[i]));
+        }
+        MyListRenewal();
+    }
+    #endregion
+    
+    
+    #region 채팅
+    public void Send()
+    {
+        PV.RPC("ChatRPC", RpcTarget.All, PhotonNetwork.NickName + " : " + ChatInput.text);
+        ChatInput.text = "";
+    }
+    
+    [PunRPC] // RPC는 플레이어가 속해있는 방 모든 인원에게 전달한다
+    void ChatRPC(string msg)
+    {
+        // for (int i = 0; i < ChatText.Length; i++)
+        //     if (ChatText[i].text == "")
+        //     {
+        //         isInput = true;
+        //         ChatText[i].text = msg;
+        //         break;
+        //     }
+        // if (!isInput) // 꽉차면 한칸씩 위로 올림
+        // {
+        //     for (int i = 1; i < ChatText.Length; i++) ChatText[i - 1].text = ChatText[i].text;
+        //     ChatText[ChatText.Length - 1].text = msg;
+        // }
+
+        if (ChatText[0].text != "") // 첫번 째 칸에 저장된 메시지가 있을 때
+        {
+            for (int i = ChatText.Length - 1; i > 0; i--)   // 메시지의 내용을 윗칸으로 한칸씩 옮긴다.
+            {
+                ChatText[i].text = ChatText[i - 1].text;
+            }
+            ChatText[0].text = msg;
+        }
+        else if(ChatText[0].text == "")// // 첫번 째 칸에 저장된 메시지가 없을 때
+        {
+            ChatText[0].text = msg; // 그대로 메시지를 넣어준다.
+        }
+        else
+        {
+            Debug.Log($"[장시진] ChatText[0].text 예외 발생");
+        }
+    }
+    #endregion
+    
+
+    #region 방
+    public void CreateRoom() => PhotonNetwork.CreateRoom(RoomInput.text == "" ? "Room" + Random.Range(0, 100) : RoomInput.text, new RoomOptions { MaxPlayers = 4 });
+    
+    public void JoinRandomRoom() => PhotonNetwork.JoinRandomRoom();
+    
+    public void LeaveRoom() => PhotonNetwork.LeaveRoom();
+    
+    public override void OnJoinedRoom()
+    {
+        // 방(Room) 화면
+        RoomPanel.SetActive(true);
+        RoomRenewal();
+        ChatInput.text = "";
+        for (int i = 0; i < ChatText.Length; i++) ChatText[i].text = "";
+        
+        // 방(Room) BGM 재생
+        SoundManager.instance.BackGroundSoundPlay(RoomPanel);
+    }
+    
+    public override void OnCreateRoomFailed(short returnCode, string message) { RoomInput.text = ""; CreateRoom(); } 
+    
+    public override void OnJoinRandomFailed(short returnCode, string message) { RoomInput.text = ""; CreateRoom(); }
+    
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + newPlayer.NickName + "님이 참가하셨습니다</color>");
+    }
+    
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        RoomRenewal();
+        ChatRPC("<color=yellow>" + otherPlayer.NickName + "님이 퇴장하셨습니다</color>");
+    }
+    
+    void RoomRenewal()
+    {
+        ListText.text = "";
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+            ListText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
+        RoomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / " + PhotonNetwork.CurrentRoom.MaxPlayers + "최대";
+    }
+    #endregion
+}
---
+++ Assets/Manager/ScenesManager.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager : MonoBehaviour
+{
+    // 불러올 로딩 씬
+    public string loadingSceneName;
+
+    public string loadSceneName;
+    public string unLoadSceneName;
+    // 불러올 다음 씬
+    [SerializeField]
+    private string nextSceneName;
+
+    private Scene[] SceneSetup;
+
+    private bool checkSceneLoding = false;
+    
+    public static ScenesManager instance; // Scenes Manager를 싱글톤으로 관리
+    private void Awake()
+    {
+        // Scenes Manager 싱글톤 설정
+        if (instance == null)
+        {
+            // SceneSetup = new Scene[SceneManager.sceneCount];
+                
+            instance = this;
+            DontDestroyOnLoad(instance);
+        } 
+        else
+        { 
+            Destroy(gameObject);  
+        }
+    }
+
+    private void Start()
+    {
+        // for (int scene = 0; scene < SceneManager.sceneCount; scene++)
+        // {
+        //     SceneSetup[scene] = SceneManager.GetSceneAt(scene);
+        // }
+        
+        // SceneManager.LoadSceneAsync(loadingSceneName, LoadSceneMode.Single);
+        // Debug.Log($"[장시진] 로딩씬 로드중...");
+        
+        // StartCoroutine(LoadScene());
+    }
+
+    private void Update()
+    {
+        if (checkSceneLoding == true)
+        {
+            StartCoroutine(LoadingScene());
+            checkSceneLoding = false;
+        }
+    }
+
+    public void LoadScene()
+    {
+        checkSceneLoding = true;
+        nextSceneName = loadSceneName;
+    }
+    
+    public void UnLoadScene()
+    {
+        checkSceneLoding = true;
+        nextSceneName = unLoadSceneName;
+    }
+
+    IEnumerator LoadingScene()
+    {
+        // 프로그램은 현재 장면이 실행될 때 백그라운드로 장면을 로드합니다.
+        // 이 기능은 로딩 화면을 만드는 데 특히 유용합니다.
+        // 씬빌드를 사용하여 씬(scene)을 로드할 수도 있습니다.
+        // 이 경우 장면2에는 씬(scene)Build가 있습니다.
+        // 빌드 설정에 표시된 대로 1의 인덱스입니다.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(nextSceneName);
+        Debug.Log($"[장시진] 다음씬 로드중...");
+
+        // 비동기 장면이 완전히 로드될 때까지 기다립니다.
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
